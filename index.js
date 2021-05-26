c1e1de632ce2bf5e7aa06fec3cc2c718e59a9eb5
Alex Gilvarry <agilvarry@utah.gov>
2021-05-26 12:58:08 -0600

    layer select

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /**
  * TODO:
+ * style layer select
  * Table
  * Zoomer
  * Search
@@ -8,65 +9,69 @@
 
 require([
   "esri/widgets/FeatureTable",
-  "esri/widgets/Legend",
   "esri/widgets/Expand",
-  "esri/widgets/LayerList",
-  "esri/layers/GroupLayer",
   "esri/layers/FeatureLayer",
   "esri/Map",
   "esri/views/MapView",
 ], function (
   FeatureTable,
-  Legend,
   Expand,
-  LayerList,
-  GroupLayer,
   FeatureLayer,
   Map,
   MapView
 ) {
-
+  selectorClass = document.querySelectorAll(".layerSelect");
   const layerQueries = {
-
-    "Finished": {
+    Finished: {
       definitionExpression:
-      "PIN_STAT_NM = 'Physically Complete' OR PIN_STAT_NM = 'Contract Complete' OR PIN_STAT_NM = 'Region Review' OR PIN_STAT_NM = 'Central Review' OR PIN_STAT_NM = 'Contract Closed Out' OR PIN_STAT_NM = 'Close Out' OR PIN_STAT_NM = 'Closed'",
-      color: "red"
+        "PIN_STAT_NM = 'Physically Complete' OR PIN_STAT_NM = 'Contract Complete' OR PIN_STAT_NM = 'Region Review' OR PIN_STAT_NM = 'Central Review' OR PIN_STAT_NM = 'Contract Closed Out' OR PIN_STAT_NM = 'Close Out' OR PIN_STAT_NM = 'Closed'",
+      color: "red",
     },
     "In Design": {
       definitionExpression:
-      "PROJ_TYP_NM <> 'Studies' AND ( PIN_STAT_NM = 'Active' OR PIN_STAT_NM = 'Concept Scoping' OR PIN_STAT_NM = 'Advertised' OR PIN_STAT_NM = 'Awarded' OR PIN_STAT_NM = 'Concept Active' OR PIN_STAT_NM = 'Concept Cmplt' OR PIN_STAT_NM = 'Scoping')",
-      color: "yellow"
+        "PROJ_TYP_NM <> 'Studies' AND ( PIN_STAT_NM = 'Active' OR PIN_STAT_NM = 'Concept Scoping' OR PIN_STAT_NM = 'Advertised' OR PIN_STAT_NM = 'Awarded' OR PIN_STAT_NM = 'Concept Active' OR PIN_STAT_NM = 'Concept Cmplt' OR PIN_STAT_NM = 'Scoping')",
+      color: "yellow",
     },
-    "Planned":{
+    Planned: {
       definitionExpression:
-      "PROJ_TYP_NM <> 'Studies' AND ( PIN_STAT_NM = 'STIP' OR PIN_STAT_NM = 'Funding' OR (PIN_STAT_NM = 'Proposed' AND REGION_PRTY <999))",
-      color: "black"
+        "PROJ_TYP_NM <> 'Studies' AND ( PIN_STAT_NM = 'STIP' OR PIN_STAT_NM = 'Funding' OR (PIN_STAT_NM = 'Proposed' AND REGION_PRTY <999))",
+      color: "black",
     },
-    "Studies":{
+    Studies: {
       definitionExpression:
-      "(PUBLIC_DESC LIKE '%stud%' OR PUBLIC_DESC LIKE '%STUD%' OR PUBLIC_DESC LIKE '%Stud%' OR PIN_DESC LIKE '%stud%' OR PIN_DESC LIKE '%STUD%' OR PIN_DESC LIKE '%Stud%' OR PROJ_TYP_NM = 'Studies') AND (PIN_STAT_NM <> 'Physically Complete' AND PIN_STAT_NM <> 'Contract Complete' AND PIN_STAT_NM <> 'Region Review' AND PIN_STAT_NM <> 'Central Review' AND PIN_STAT_NM <> 'Contract Closed Out' AND PIN_STAT_NM <> 'Close Out' AND PIN_STAT_NM <> 'Closed')",
-      color: "blue"
+        "(PUBLIC_DESC LIKE '%stud%' OR PUBLIC_DESC LIKE '%STUD%' OR PUBLIC_DESC LIKE '%Stud%' OR PIN_DESC LIKE '%stud%' OR PIN_DESC LIKE '%STUD%' OR PIN_DESC LIKE '%Stud%' OR PROJ_TYP_NM = 'Studies') AND (PIN_STAT_NM <> 'Physically Complete' AND PIN_STAT_NM <> 'Contract Complete' AND PIN_STAT_NM <> 'Region Review' AND PIN_STAT_NM <> 'Central Review' AND PIN_STAT_NM <> 'Contract Closed Out' AND PIN_STAT_NM <> 'Close Out' AND PIN_STAT_NM <> 'Closed')",
+      color: "blue",
     },
-    "Construction":{
+    Construction: {
       definitionExpression:
-      "PROJ_TYP_NM <> 'Studies' AND (PIN_STAT_NM = 'Under Construction')",
-      color: "green"
+        "PROJ_TYP_NM <> 'Studies' AND (PIN_STAT_NM = 'Under Construction')",
+      color: "green",
+    },
+    "Substantially Complete": {
+      definitionExpression:
+        "PROJ_TYP_NM <> 'Studies' AND (PIN_STAT_NM = 'Under Construction')",
+      color: "orange",
+    },
+    "All Projects": {
+      definitionExpression: "1=1",
+      color: "purple",
+    },
+  };
+  selectorClass.forEach((e) => {
+    e.addEventListener("click", swapLayer);
+  });
 
-    },
-    "Substantially Complete":{
-      definitionExpression:
-      "PROJ_TYP_NM <> 'Studies' AND (PIN_STAT_NM = 'Under Construction')",
-      color: "orange"
-    },
-    "All Projects"
-
-
+  function swapLayer(e) {
+    const query = layerQueries[e.target.id].definitionExpression;
+    const color = layerQueries[e.target.id].color;
+    lines.definitionExpression = query;
+    lines.renderer.symbol.color = color;
+    points.definitionExpression = query;
+    points.renderer.symbol.color = color;
   }
 
   const lines = new FeatureLayer({
     url: "https://maps.udot.utah.gov/central/rest/services/EPM/EPM_All_Projects_as_Lines/MapServer/0",
-    title: "All Projects - Lines",
     renderer: {
       type: "simple",
       symbol: { type: "simple-line", width: 2, color: "purple" },
@@ -74,7 +79,6 @@ require([
   });
   const points = new FeatureLayer({
     url: "https://maps.udot.utah.gov/central/rest/services/EPM/EPM_AllProjectsPoints/MapServer/0",
-    title: "All Projects - Points",
     renderer: {
       type: "simple",
       symbol: { type: "simple-marker", size: 6, color: "purple" },
@@ -83,15 +87,7 @@ require([
 
   const map = new Map({
     basemap: "gray",
-    layers: [
-      finished,
-      inDesign,
-      planned,
-      studies,
-      construction,
-      substComplt,
-      all,
-    ],
+    layers: [lines, points],
   });
 
   const view = new MapView({
@@ -100,45 +96,18 @@ require([
     zoom: 13, // Zoom level
     container: "viewDiv", // Div element
   });
-  view.popup.defaultPopupTemplateEnabled = true
-  const layerList = new LayerList({ view: view });
+  view.popup.defaultPopupTemplateEnabled = true;
 
-  const legend = new Legend({
-    view: view,
-    layerInfos: [
-      {
-        layer: finished,
-      },
-      {
-        layer: inDesign,
-      },
-      {
-        layer: planned,
-      },
-      {
-        layer: studies,
-      },
-      {
-        layer: construction,
-      },
-      {
-        layer: substComplt,
-      },
-      {
-        layer: all,
-      },
-    ],
-  });
-  const legendExpand = new Expand({
-    view: view,
-    content: legend,
-    group: "top-left",
-  });
   const listExpand = new Expand({
     view: view,
-    content: layerList,
+    expanded: true,
+    content: document.getElementById("layerQuery"),
     group: "top-left",
   });
-
-  view.ui.add([listExpand, legendExpand], "top-left");
+  const featureTable = new FeatureTable({
+    layer: lines,
+    view: view,
+    container: document.getElementById("tableDiv")
+  })
+  view.ui.add(listExpand, "top-left");
 });
