6bd35fd0b3f559b762e594f3c15a72ce8aa35dd6
Alex Gilvarry <agilvarry@utah.gov>
2021-08-02 12:29:12 -0600

    projects app

---
+++ index.js
@@ -0,0 +1,171 @@
+/**
+ * TODO:
+
+ * Zoomer
+ * Search
+ * Inset Map Location
+ * style layer select
+ * Table
+ */
+
+require([
+  // "dijit/layout/BorderContainer",
+  // "dijit/layout/ContentPane",
+  "esri/widgets/FeatureTable",
+  "esri/widgets/Expand",
+  "esri/layers/FeatureLayer",
+  "esri/Map",
+  "esri/views/MapView",
+], function (
+  // BorderContainer,
+  // ContentPane,
+  FeatureTable,
+  Expand,
+  FeatureLayer,
+  Map,
+  MapView
+) {
+  selectorClass = document.querySelectorAll(".layerSelect");
+  const layerQueries = {
+    Finished: {
+      definitionExpression:
+        "PIN_STAT_NM = 'Physically Complete' OR PIN_STAT_NM = 'Contract Complete' OR PIN_STAT_NM = 'Region Review' OR PIN_STAT_NM = 'Central Review' OR PIN_STAT_NM = 'Contract Closed Out' OR PIN_STAT_NM = 'Close Out' OR PIN_STAT_NM = 'Closed'",
+      color: "red",
+    },
+    "In Design": {
+      definitionExpression:
+        "PROJ_TYP_NM <> 'Studies' AND ( PIN_STAT_NM = 'Active' OR PIN_STAT_NM = 'Concept Scoping' OR PIN_STAT_NM = 'Advertised' OR PIN_STAT_NM = 'Awarded' OR PIN_STAT_NM = 'Concept Active' OR PIN_STAT_NM = 'Concept Cmplt' OR PIN_STAT_NM = 'Scoping')",
+      color: "yellow",
+    },
+    Planned: {
+      definitionExpression:
+        "PROJ_TYP_NM <> 'Studies' AND ( PIN_STAT_NM = 'STIP' OR PIN_STAT_NM = 'Funding' OR (PIN_STAT_NM = 'Proposed' AND REGION_PRTY <999))",
+      color: "black",
+    },
+    Studies: {
+      definitionExpression:
+        "(PUBLIC_DESC LIKE '%stud%' OR PUBLIC_DESC LIKE '%STUD%' OR PUBLIC_DESC LIKE '%Stud%' OR PIN_DESC LIKE '%stud%' OR PIN_DESC LIKE '%STUD%' OR PIN_DESC LIKE '%Stud%' OR PROJ_TYP_NM = 'Studies') AND (PIN_STAT_NM <> 'Physically Complete' AND PIN_STAT_NM <> 'Contract Complete' AND PIN_STAT_NM <> 'Region Review' AND PIN_STAT_NM <> 'Central Review' AND PIN_STAT_NM <> 'Contract Closed Out' AND PIN_STAT_NM <> 'Close Out' AND PIN_STAT_NM <> 'Closed')",
+      color: "blue",
+    },
+    Construction: {
+      definitionExpression:
+        "PROJ_TYP_NM <> 'Studies' AND (PIN_STAT_NM = 'Under Construction')",
+      color: "green",
+    },
+    "Substantially Complete": {
+      definitionExpression:
+        "PROJ_TYP_NM <> 'Studies' AND (PIN_STAT_NM = 'Substantially Compl')",
+      color: "orange",
+    },
+    "All Projects": {
+      definitionExpression: "1=1",
+      color: "purple",
+    },
+  };
+
+
+  let linesView;
+  const linesLayer = new FeatureLayer({
+    url: "https://maps.udot.utah.gov/central/rest/services/EPM/EPM_All_Projects_as_Lines/MapServer/0",
+    definitionExpression:
+        "PROJ_TYP_NM <> 'Studies' AND ( PIN_STAT_NM = 'STIP' OR PIN_STAT_NM = 'Funding' OR (PIN_STAT_NM = 'Proposed' AND REGION_PRTY <999))",
+    renderer: {
+      type: "simple",
+      symbol: { type: "simple-line", width: 2, color: "black" },
+    },
+  });
+
+
+  let pointsView;
+  const pointsLayer = new FeatureLayer({
+    url: "https://maps.udot.utah.gov/central/rest/services/EPM/EPM_AllProjectsPoints/MapServer/0",
+    definitionExpression:
+        "PROJ_TYP_NM <> 'Studies' AND ( PIN_STAT_NM = 'STIP' OR PIN_STAT_NM = 'Funding' OR (PIN_STAT_NM = 'Proposed' AND REGION_PRTY <999))",
+    renderer: {
+      type: "simple",
+      symbol: { type: "simple-marker", size: 6, color: "black" },
+    },
+  });
+
+  const map = new Map({
+    basemap: "gray",
+    layers: [linesLayer, pointsLayer],
+  });
+
+  const view = new MapView({
+    map: map,
+    center: [-111.891, 40.7608], // Longitude, latitude
+    zoom: 13, // Zoom level
+    container: "viewDiv", // Div element
+  });
+  view.popup.defaultPopupTemplateEnabled = true;
+
+  view.whenLayerView(linesLayer).then(function(layer){
+    linesView = layer;
+
+    const featureTable = new FeatureTable({
+      layer: linesLayer,
+      view: view,
+      container: document.getElementById("tableDiv")
+    });
+
+    linesLayer.watch("loaded", () => {
+      watchUtils.whenFalse(view, "updating", () => {
+        // Get the new extent of view/map whenever map is updated
+        if (view.extent) {
+          // Filter and show only the visible features in the feature table
+          featureTable.filterGeometry = view.extent;
+        }
+      });
+    });
+  });
+  view.whenLayerView(pointsLayer).then(function(layer){
+    pointsView = layer;
+  });
+
+
+  const listExpand = new Expand({
+    view: view,
+    expanded: true,
+    content: document.getElementById("layerQuery"),
+    group: "top-left"
+  });
+  const zoomerExpand = new Expand({
+    view: view,
+    expanded: false,
+    content:document.getElementById("zoomer"),
+    group: "top-left"
+  });
+
+  
+  view.ui.add([listExpand, zoomerExpand], "top-left");
+
+
+  /**Zoomer Code */
+  const zoomButtons = document.querySelectorAll(".zoomer-button-entry");
+
+  zoomButtons.forEach((e) => {
+    e.addEventListener("click", toggleZoomTool);
+  });
+
+  function toggleZoomTool(event){
+    console.log(event)
+  }
+
+  selectorClass.forEach((e) => {
+    e.addEventListener("click", swapLayer);
+  });
+  const featureFilter = {
+    where: "1=1"
+  }
+  function swapLayer(e) {
+    const query = layerQueries[e.target.id].definitionExpression;
+    const color = layerQueries[e.target.id].color;
+    filter = {where: query};
+    linesView.filter = filter;
+    linesLayer.renderer.symbol.color = color;
+    pointsView.filter = filter;
+    pointsLayer.renderer.symbol.color = color;
+    linesLayer.definitionExpression = query;
+  };
+});
